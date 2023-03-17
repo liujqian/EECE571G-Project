6a26e0f1da204df3e61a538f09f75a1055ccbd6f
acmusgrave <andrew.musgrave97@gmail.com>
2023-03-17 12:43:12 -0700

    Added function signatures for a few important functions

--- src/Bingo571G.sol
+++ src/Bingo571G.sol
@@ -9,7 +9,7 @@ struct Card{
 }
 
 struct Game {
-    address payable host;
+    address payable host_address;
     uint host_fee; // in percent
     uint start_time; // Unix timestamp of start time (must be in the future)
     uint turn_time; // time between draws
@@ -27,5 +27,27 @@ contract BingoEECE571G {
 
     address payable public constant dev_address = payable(address(0x100)); // change before deployment
     mapping (address => Game) games; // indexed by host address: can we do this and allow each address to host at most one game to keep things simple?
+    mapping (address => address[]) player_games; // allows players to find the host addresses of their active games (can remove later if too gas intensive)
+
+
+    // creates a new game with msg.sender as host
+    function createGame(uint host_fee, uint start_time, uint turn_time) public{
+        return;
+    }
+
+    // Draws next number for game with host address msg.sender, if it has been long enough since last draw
+    function hostDrawNumber() public {
+        return;
+    }
+
+    // returns number of BINGOs for a card defined by game address and index
+    function checkCard(address game_host, address player, uint index) public view returns(uint){
+        return 0;
+    }
+
+    function checkGameStatus(address host) public view returns(bool is_valid, uint[] memory numbers){
+        
+        return(false, new uint[](0));
+    }
 
 }

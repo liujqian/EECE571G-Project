5df85f50b259c8755ce88ae2ac500c614975d6bb
Taha Shabani <taha.shabani.m@gmail.com>
2023-03-19 02:08:58 -0700

    generate random number for each iteration

--- src/Bingo571G.sol
+++ src/Bingo571G.sol
@@ -10,10 +10,10 @@ struct Card{
 
 struct Game {
     address payable host_address;
-    uint host_fee; // in percent
-    uint start_time; // Unix timestamp of start time (must be in the future)
-    uint turn_time; // time between draws
-    uint last_draw_time;
+    uint256 host_fee; // in percent
+    uint256 start_time; // Unix timestamp of start time (must be in the future)
+    uint256 turn_time; // time between draws
+    uint256 last_draw_time;
     address[] players; // array of players, which we can iterate over to check for a winner
     mapping (address => Card[]) player_cards; // mapping of addresses to array of cards, so players can check their cards easily
     uint[] numbers_drawn; // initialized with 0 in first entry for every game (free tile)
@@ -26,18 +26,31 @@ struct Game {
 contract BingoEECE571G {
 
     address payable public constant dev_address = payable(address(0x100)); // change before deployment
-    mapping (address => Game) games; // indexed by host address: can we do this and allow each address to host at most one game to keep things simple?
-    mapping (address => address[]) player_games; // allows players to find the host addresses of their active games (can remove later if too gas intensive)
+    mapping (address => Game) private games; // indexed by host address: can we do this and allow each address to host at most one game to keep things simple?
+    mapping (address => address[]) private player_games; // allows players to find the host addresses of their active games (can remove later if too gas intensive)
 
+    modifier hostExists(address _sender) {
+        require(games[_sender].start_time > 0, "Host doesn't exist!");
+        _;
+    }
 
+    modifier validInterval(address _sender) {
+        require(games[_sender].last_draw_time + games[_sender].turn_time < block.timestamp, 
+        "Not enough time has passed to draw a new number!");
+        _;
+    }
+    
+    
     // creates a new game with msg.sender as host
     function createGame(uint host_fee, uint start_time, uint turn_time) public{
         return;
     }
 
     // Draws next number for game with host address msg.sender, if it has been long enough since last draw
-    function hostDrawNumber() public {
-        return;
+    function hostDrawNumber() public hostExists(msg.sender){
+        uint random_number = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 100;
+
+        games[msg.sender].numbers_drawn.push(random_number);
     }
 
     // returns number of BINGOs for a card defined by game address and index

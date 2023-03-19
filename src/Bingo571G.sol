9cba4ba2cd171e9a1d5bd673abd41b5d4f5c5d70
acmusgrave <andrew.musgrave97@gmail.com>
2023-03-19 15:24:50 -0700

    Added createGame implementation

--- src/Bingo571G.sol
+++ src/Bingo571G.sol
@@ -10,13 +10,14 @@ struct Card{
 
 struct Game {
     address payable host_address;
-    uint256 host_fee; // in percent
+    uint256 host_fee; // in Wei per Eth of pool value (fraction of pool x 10^18)
     uint256 start_time; // Unix timestamp of start time (must be in the future)
     uint256 turn_time; // time between draws
     uint256 last_draw_time;
     address[] players; // array of players, which we can iterate over to check for a winner
     mapping (address => Card[]) player_cards; // mapping of addresses to array of cards, so players can check their cards easily
     uint[] numbers_drawn; // initialized with 0 in first entry for every game (free tile)
+    bool is_valid;
     bool has_started;
     bool has_completed;
     uint pool_value;
@@ -42,7 +43,33 @@ contract BingoEECE571G {
     
     
     // creates a new game with msg.sender as host
-    function createGame(uint host_fee, uint start_time, uint turn_time) public{
+    function createGame(uint _host_fee, uint _start_time, uint _turn_time) public{
+        require(_start_time > block.timestamp, "Start time must be in the future.");
+        if(games[msg.sender].is_valid && !games[msg.sender].has_completed){
+            revert("Must wait until existing game has completed.");
+        }
+        games[msg.sender].host_address = payable(msg.sender);
+        games[msg.sender].host_fee = _host_fee;
+        games[msg.sender].start_time = _start_time;
+        games[msg.sender].turn_time = _turn_time;
+        games[msg.sender].last_draw_time = 0;
+        games[msg.sender].numbers_drawn = [0]; // Initialize with 0 (free square)
+        games[msg.sender].has_started = false;
+        games[msg.sender].has_completed = false;
+        games[msg.sender].pool_value = 0;
+
+        // clear all data from previous games
+        for(uint i=0; i<games[msg.sender].players.length; i++){
+
+            address a = games[msg.sender].players[i];
+            games[msg.sender].player_cards[a] = new Card[](0);
+        }
+
+        games[msg.sender].players = new address[](0);
+
+        games[msg.sender].is_valid = true;
+
+
         return;
     }
 
@@ -54,15 +81,14 @@ contract BingoEECE571G {
     }
 
     // returns number of BINGOs for a card defined by game address and index
-    function checkCard(address game_host, address player, uint index) public view returns(uint){
-        games[game_host].player_cards[player][index].numbers; // how to check bingos?
+    function checkCard(address _game_host, address player, uint index) public view returns(uint){
         uint8[5] memory columns = [1, 1, 1, 1, 1];
         uint8[5] memory rows = [1, 1, 1, 1, 1];
         uint down_diagonal = 1;
         uint up_diagonal = 1;
         
         for(uint i=0; i<25; i++){
-            if(!isPresent(games[game_host].player_cards[player][index].numbers[i], games[game_host].numbers_drawn)){
+            if(!isPresent(games[_game_host].player_cards[player][index].numbers[i], games[_game_host].numbers_drawn)){
                 columns[i/5] = 0;
                 rows[i%5] = 0;
                 if(i/5 == i%5){
